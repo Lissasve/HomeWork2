411ae185fa8b55d3cc8b72646a40a62a5dd5495f
Module_0_2 <Lisasve546@gmail.com>
2024-12-01 17:17:56 +0700

    Модуль 1 Tema Строки и индексация строк

--- strokiindexacia.py
+++ strokiindexacia.py
@@ -1,6 +1,6 @@
 example = "Апельсин"
-print (example [0])
-print(example [-1])
-print(example [4:])
-print(example [::-1])
-print(example [1:8:2])
+print (example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1:2])
