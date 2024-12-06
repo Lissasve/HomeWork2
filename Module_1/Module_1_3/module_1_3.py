eafa121a672d16e064df48907fcf2cb9c8b66535
Lissasve <Lisasve546@gmail.com>
2024-12-06 10:37:50 +0700

    Delete Module_1/Module_1_3 directory

--- Module_1/Module_1_3/module_1_3.py
+++
@@ -1,7 +0,0 @@
-name = "Svetlana"
-print('Name:',name)
-age = 45
-print('Age:',age)
-print('New_age:',age+1)
-is_student = True
-print('Is_Student:',is_student)
--- Module_1/Module_1_3/module_1_5.py
+++
@@ -1,8 +0,0 @@
-immutable_var = tuple(['x', 'y', 5, 7])
-print(f'Immutable tuple: {immutable_var}')
-# immutable_var[1] = 'a'
-# print(immutable_var)
-mutable_list = [7, 8, 'a', 'b', 'orange']
-mutable_list[4] = 'cherry'
-print(f'Mutable list: {mutable_list}')
-
