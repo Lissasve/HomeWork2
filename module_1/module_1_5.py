22ecbc79833b3b4e0c579b7410f87cce1b16d888
Module_0_2 <Lisasve546@gmail.com>
2024-12-06 11:12:27 +0700

    Модуль 1 Тема Неизменяемые и изменяемые объекты. Кортежи Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"
    Цель:
    
    Написать программу на языке Python, используя Pycharm, для работы с неизменяемыми и изменяемыми объектами.
    
    1. В проекте, где вы решаете домашние задания, создайте модуль 'module_1_5.py' и напишите весь код в нём.
    
    2. Задайте переменные разных типов данных:
    
      - Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
    
      - Выполните операции вывода кортежа immutable_var на экран.
    
    3. Изменение значений переменных:
    
      - Попытайтесь изменить элементы кортежа immutable_var. Объясните, почему нельзя изменить значения элементов кортежа.
    
    4. Создание изменяемых структур данных:
    
      - Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
    
      - Измените элементы списка mutable_list.
    
      - Выведите на экран измененный список mutable_list.
    
    Примечания:
    
    - Для вывода значений на экран используйте функцию print().
    
    - Обратите внимание на особенности изменяемых и неизменяемых объектов в Python.
    
    Пример результата выполнения программы:
    
    Immutable tuple: (1, 2, 'a', 'b')
    
    Mutable list: [1, 2, 'a', 'b', 'Modified']

---
+++ module_1/module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = tuple(['x', 'y', 5, 7])
+print(f'Immutable tuple: {immutable_var}')
+# immutable_var[1] = 'a'
+# print(immutable_var)
+mutable_list = [7, 8, 'a', 'b', 'orange']
+mutable_list[4] = 'cherry'
+print(f'Mutable list: {mutable_list}')
+
